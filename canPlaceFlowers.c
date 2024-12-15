95c15e1e015d4eb1fc303eae8dd69746d7580ce2
douglas020 <103688515+douglas020@users.noreply.github.com>
2024-12-14 21:10:42 -0300

    Add files via upload

---
+++ canPlaceFlowers.c
@@ -0,0 +1,26 @@
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+    if (n <0)
+    return false;
+
+    if (n > flowerbedSize)
+    return false;
+
+    if (n == flowerbedSize)
+    return true;
+
+    if (n == 0){
+        return true;
+    }
+
+    if (flowerbed[n-1] == 1 &&  flowerbed[ +1] == 0)
+    return true;
+
+    if (flowerbed[n-1] == 0 &&  flowerbed[ +1] == 1)
+    return true;
+
+
+
+
+    return false;
+
+}
